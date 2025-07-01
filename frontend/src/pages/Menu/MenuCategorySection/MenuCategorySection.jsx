4159b8ce60c036bfdc5d0481cc296d254ef9b871
ammarhashmi113 <ammarhashmi113@gmail.com>
2025-07-01 19:53:10 +0500

    Update user's menu category section UI

--- frontend/src/pages/Menu/MenuCategorySection/MenuCategorySection.jsx
+++ frontend/src/pages/Menu/MenuCategorySection/MenuCategorySection.jsx
@@ -4,13 +4,25 @@ import MenuCard from "./MenuCard/MenuCard";
 const MenuCategorySection = ({ title, imageUrl, items, onSelect, isAdmin }) => {
     return (
         <section className="mb-12">
-            <div className="relative w-full h-64 mb-6 rounded-sm overflow-hidden shadow">
+            {/* <div className="relative w-full h-64 mb-6 rounded-sm overflow-hidden shadow">
                 <img
                     src={imageUrl}
                     alt={title}
                     className="w-full h-full object-cover brightness-60"
                 />
                 <h2 className="absolute inset-0 flex items-center justify-center text-3xl sm:text-4xl font-bold text-white text-center font-serif">
+                    {title}
+                </h2>
+            </div> */}
+
+            <div className="relative w-full aspect-[16/5] sm:h-64 mb-6 rounded-sm overflow-hidden shadow">
+                <img
+                    src={imageUrl}
+                    alt={title}
+                    className="w-full h-full object-cover brightness-60"
+                />
+                <div className="absolute inset-0 bg-gradient-to-r from-red-600/40 to-transparent z-10" />
+                <h2 className="absolute inset-0 flex items-center justify-center text-3xl sm:text-4xl font-bold text-white text-center font-serif drop-shadow-md z-20">
                     {title}
                 </h2>
             </div>
