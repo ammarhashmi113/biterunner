7c3e32e5474306e27c345486bcef6ba4c532527b
ammarhashmi113 <ammarhashmi113@gmail.com>
2025-07-18 01:38:57 +0500

    ui: FirstVisitModal UI changes

--- frontend/src/components/FirstVisitModal.jsx
+++ frontend/src/components/FirstVisitModal.jsx
@@ -30,13 +30,13 @@ const FirstVisitModal = () => {
 
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-[rgba(0,0,0,0.3)]">
-            <div className="bg-white text-black rounded-2xl p-6 max-w-md w-full shadow-xl text-center border-2 border-red-600">
-                <h2 className="text-2xl font-bold text-red-600 mb-3">
+            <div className="bg-white text-black rounded-2xl p-6 max-w-md w-full shadow-xl text-center">
+                <h2 className="text-2xl font-bold text-red-500 mb-3">
                     Backend Notice
                 </h2>
                 <p className="mb-4 text-gray-800">
-                    Our backend API is currently{" "}
-                    <span className="font-semibold text-red-600">offline</span>{" "}
+                    Biterunner's API is currently{" "}
+                    <span className="font-semibold text-red-500">offline</span>{" "}
                     due to free hosting limits. So some features like viewing
                     the menu, placing orders, or logging in won’t work for now.
                 </p>
@@ -44,7 +44,7 @@ const FirstVisitModal = () => {
                     You can still explore the UI and check out the full code on{" "}
                     <a
                         href="https://github.com/ammarhashmi113/biterunner"
-                        className="text-red-600 underline"
+                        className="text-red-500 underline"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
@@ -54,7 +54,7 @@ const FirstVisitModal = () => {
                 </p>
                 <button
                     onClick={() => setShowModal(false)}
-                    className="bg-red-600 text-white px-5 py-2 rounded-md hover:bg-red-700 transition cursor-pointer"
+                    className="bg-red-500 text-white px-5 py-2 rounded-md hover:bg-red-600 transition cursor-pointer"
                 >
                     Continue to site
                 </button>
